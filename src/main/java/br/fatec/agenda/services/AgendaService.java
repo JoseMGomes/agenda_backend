d929a9352bc27e100f000cba15bcf7f2ed411231
José Lucas <gomessze03@gmail.com>
2024-11-29 12:17:54 -0300

    created sevices

--- src/main/java/br/fatec/agenda/services/AgendaService.java
+++ src/main/java/br/fatec/agenda/services/AgendaService.java
@@ -1,12 +1,56 @@
 package br.fatec.agenda.services;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.stream.Collectors;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@SpringBootApplication
+import br.fatec.agenda.mappers.AgendaMappers;
+import br.fatec.agenda.repository.AgendaRepository;
+import jakarta.persistence.EntityNotFoundException;
+import br.fatec.agenda.dtos.AgendaRequest;
+import br.fatec.agenda.dtos.AgendaResponse;
+import br.fatec.agenda.entities.Agenda;
+
+@Service
 public class AgendaService {
-    
-    public static void main(String[] args){
-        SpringApplication.run(AgendaService.class, args);
+
+    @Autowired
+    private AgendaRepository repository;
+
+    public List<AgendaResponse> getAllAgendas() {
+        return repository.findAll().stream().map(a -> AgendaMappers.toDTO(a)).collect(Collectors.toList());
     }
+
+    public AgendaResponse getAgendaById(long id) {
+        Agenda agenda = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Produto não encontrado"));
+
+        return AgendaMappers.toDTO(agenda);
+    }
+
+    public void delete(long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Produto não encontrado");
+        }
+    }
+
+    public AgendaResponse save(AgendaRequest agenda) {
+        Agenda newAgenda = repository.save(AgendaMappers.toEntity(agenda));
+        return AgendaMappers.toDTO(newAgenda);
+    }
+
+    public void update(AgendaRequest agenda, long id) {
+        Agenda aux = repository.getReferenceById(id);
+
+        aux.setDescription(agenda.description());
+        aux.setDuration(agenda.duration());
+        aux.setLocal(agenda.local());
+
+        repository.save(aux);
+
+    }
+
 }
