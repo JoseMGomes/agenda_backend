d4c5cc1ced90e9bf1548c50bec60d6ebb866b086
José Lucas <gomessze03@gmail.com>
2024-11-29 11:29:14 -0300

    creating data within the database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Consulta de planejamento financeiro', '90 min', 'Campinas - SP');
+INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Encontro com amigos', '120 min', 'Sorocaba - SP');
+INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Aula de inglês', '60 min', 'Online');
+INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Treinamento na academia', '45 min', 'Itu - SP');
+INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Palestra sobre tecnologia', '180 min', 'São Paulo - SP');
+INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Sessão de terapia', '50 min', 'Jundiaí - SP');
