7e9d1b6b88acc97d9f13da41e3fc6b39bf2b8831
José Lucas <gomessze03@gmail.com>
2024-12-07 17:29:28 -0300

    Fix: resolve CORS issue for local development environment

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Consulta de planejamento financeiro', '90 min', 'Campinas - SP');
-INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Encontro com amigos', '120 min', 'Sorocaba - SP');
-INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Aula de inglês', '60 min', 'Online');
-INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Treinamento na academia', '45 min', 'Itu - SP');
-INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Palestra sobre tecnologia', '180 min', 'São Paulo - SP');
-INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Sessão de terapia', '50 min', 'Jundiaí - SP');
+INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Consulta de planejamento financeiro', '10/12/2024', 'Campinas - SP');
+INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Encontro com amigos', '25/12/2024', 'Sorocaba - SP');
+INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Aula de inglês', '17/12/2024', 'Online');
+INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Treinamento na academia', '24/12/2024', 'Itu - SP');
+INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Palestra sobre tecnologia', '15/12/2024', 'São Paulo - SP');
+INSERT INTO AGENDA (DESCRIPTION, DURATION, LOCAL) VALUES ('Sessão de terapia', '30/11/2024', 'Jundiaí - SP');
